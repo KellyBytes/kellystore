0b4fe7dbcc02d8d000dcf34aa86d973a332b01e0
Kelly <kellybytes.ca@gmail.com>
2025-11-18 19:32:11 -0700

    Finish styling and integrate Stripe
    - Add components (ImageBanner, Products, Portal)
    - Finish styling with static data
    - Integrate Stripe

---
+++ app/head.js
@@ -0,0 +1,22 @@
+export default function Head() {
+  return (
+    <head>
+      <link rel="icon" href="/favicon.png" sizes="any" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Eczar:regular,500,600,700,800"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Grenze:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
+        rel="stylesheet"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
+      />
+    </head>
+  );
+}
